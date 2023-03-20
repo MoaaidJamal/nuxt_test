b1f22eadc21e742590c0aaea0e6586aea1913a9d
Muayad <mjasaada@gmail.com>
2023-03-20 13:38:35 +0200

    init commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup>
+import axios from 'axios';
+
+const config = useRuntimeConfig();
+let users = await axios.get(config.public.apiBase + '/users')
+  .then(response => {
+    return response.data
+  })
+  .catch(error => {
+    console.log(error)
+  })
+</script>
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
